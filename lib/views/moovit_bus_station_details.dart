7bf2b90d1fac581d8f3773e59051786483787edf
PythonXHunter <aryan.computerscience@gmail.com>
2023-03-26 11:27:38 +0530

    added screen

---
+++ lib/views/moovit_bus_station_details.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MoovitStationDetails extends StatelessWidget {
+  const MoovitStationDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
